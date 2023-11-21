d6ac249db0295f058988a2e5c4914ef1ebf7d6f6
Nicro01 <110069146+Nicro01@users.noreply.github.com>
2023-11-21 08:46:57 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -5,15 +5,12 @@ import googlemaps
 import pandas as pd
 import os
 
-# Replace 'YOUR_GOOGLE_MAPS_API_KEY' with your actual Google Maps API key
 API_KEY = ''
 gmaps = googlemaps.Client(key=API_KEY)
 
-# Your home view
 def home(request):
     return render(request, 'home.html')
 
-# Your index view
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
@@ -23,28 +20,26 @@ def index(request):
 
         df = search_places(city, radius, segment)
         if df is None:
-            # Handle error, maybe return an error message to the user
+          
             return render(request, 'home.html', {'error': 'Error in geocoding'})
 
-        # Call the save_csv function to initiate the download
+       
         response = save_csv(df, file_name)
 
         if response is None:
-            # Handle error, maybe return an error message to the user
+            
             return render(request, 'home.html', {'error': 'Error saving CSV file'})
 
         return response
 
     return render(request, 'home.html')
 
-# Your search_places function
 def search_places(city, radius, segment):
     try:
         geocode_result = gmaps.geocode(city)
         latitude = geocode_result[0]['geometry']['location']['lat']
         longitude = geocode_result[0]['geometry']['location']['lng']
     except:
-        # Handle error
         return None
 
     df = pd.DataFrame(columns=['codigo', 'razao_social', 'telefone_1', 'telefone_2'])
@@ -69,13 +64,11 @@ def search_places(city, radius, segment):
 
     return df
 
-# Your save_csv function
 def save_csv(df, file_name):
     if not df.empty:
-        # Create a CSV string from the DataFrame
+    
         csv_content = df.to_csv(index=False)
 
-        # Set the response content type and headers
         response = HttpResponse(csv_content, content_type='text/csv')
         response['Content-Disposition'] = f'attachment; filename={file_name}.csv'
 
